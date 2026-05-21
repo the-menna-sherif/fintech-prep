b628096088e0ac94432479ca8883f1895d99b858
the-menna-sherif <mennaseducation@gmail.com>
2026-05-21 14:55:32 +0300

    updated ingestion file with comments & dotenv intgrtn

--- EarningsIntelligenceEngine/ingestion/sec_downloader.py
+++ EarningsIntelligenceEngine/ingestion/sec_downloader.py
@@ -1,13 +1,19 @@
 from sec_edgar_downloader import Downloader
 import pathlib
+import os
+from dotenv import load_dotenv
 
+# This looks for a .env file in the current directory and loads it
+load_dotenv()
 
 # directing downloads into data/raw/ -- separate from processed data & code
 RAW_DIR = pathlib.Path(__file__).parent.parent / "data" / "raw"
 
 # define downloder function
-def download_sec_filings(ticker: str, filing_type: str, count: int = 10):
-    downloader = Downloader("menmen_fintech_prep", "mennaseducation@gmail.com")
+def download_sec_filings(ticker: str, filing_type: str, count: int = 2):
+    # initialize the downloader with credentials from environment variables
+    # credentials are required by the SEC to track usage and prevent abuse of their API (my creds just identify me as a researcher)
+    downloader = Downloader(os.getenv("COMPANY_NAME"), os.getenv("USER_EMAIL"))
 
     downloader.get(ticker, filing_type, count, output_dir=RAW_DIR)
 
@@ -16,4 +22,4 @@ def download_sec_filings(ticker: str, filing_type: str, count: int = 10):
 
 if __name__ == "__main__":
     # example usage
-    download_sec_filings("AAPL", "10-K", count=5)
+    download_sec_filings("AAPL", "10-K", count=2)
